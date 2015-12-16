9eb2fc651272f7390748343afeed37d02090013c
Edward Slavov <e.slavov11@gmail.com>
2015-12-16 15:14:55 +0200

    project added

---
+++ RPG-Armeni/RPGApp.cs
@@ -0,0 +1,19 @@
+﻿namespace SuperRpgGame
+{
+    using Engine;
+    using Interfaces;
+    using UI;
+
+    public class RpgApp
+    {
+        public static void Main()
+        {
+            IRenderer renderer = new ConsoleRenderer();
+            IInputReader reader = new ConsoleInputReader();
+
+            SuperEngine engine = new SuperEngine(reader, renderer);
+
+            engine.Run();
+        }
+    }
+}
